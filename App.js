bf7ef4aa72fb00397a39d990a1d1e86830a12066
alokgoldy <alokkrgoldy@gmail.com>
2020-08-20 06:56:36 +0530

    second commit

--- App.js
+++ App.js
@@ -1,1 +1,7 @@
-import 
+import {createAppContainer,createSwitchNavigator } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './screens/HomeScreen';
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+

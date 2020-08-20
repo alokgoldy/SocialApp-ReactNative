196176db49cebc1884299323ede78896db21dbac
alokgoldy <alokkrgoldy@gmail.com>
2020-08-20 07:15:27 +0530

    Switch Nav

--- App.js
+++ App.js
@@ -5,3 +5,41 @@ import LoadingScreen from './screens/LoadingScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCIj1INed6puWp5Ez36V8D2HRU3iQoSzhk",
+    authDomain: "socialapp-292d4.firebaseapp.com",
+    databaseURL: "https://socialapp-292d4.firebaseio.com",
+    projectId: "socialapp-292d4",
+    storageBucket: "socialapp-292d4.appspot.com",
+    messagingSenderId: "276781638537",
+    appId: "1:276781638537:web:89cab1302175dcad46f758"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+const AppStack = createStackNavigator({
+    Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+    Login: LoadingScreen,
+    Register: RegisterScreen
+})
+
+
+export default createAppContainer(
+
+    createSwitchNavigator(
+        {
+            Loading: LoadingScreen,
+            App : AppStack,
+            Auth: AuthStack
+        },
+        {
+            initialRouteName: "Loading"
+        }
+    )
+);
